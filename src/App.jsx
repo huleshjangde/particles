f33963790fff13fd0dc0ea188df4ed0c01b5ca81
huleshjangde <huleshjangde1@.com>
2023-12-28 18:45:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,128 @@
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadFull } from "tsparticles";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+    const containerRef = useRef(null), [ init, setInit ] = useState(false);
+
+    useEffect(() => {
+        if (init) {
+            return;
+        }
+
+        initParticlesEngine(async (engine) => {
+            await loadFull(engine);
+        }).then(() => {
+            setInit(true);
+        });
+    }, [ init ]);
+
+    const particlesLoaded = useCallback(
+            (container) => {
+                containerRef.current = container;
+
+                window.particlesContainer = container;
+            },
+            [ containerRef ]
+        ),
+        options = useMemo(
+            () => ({
+                fpsLimit: 120,
+                interactivity: {
+                  events: {
+                    onClick: {
+                      enable: true,
+                      mode: "push",
+                    },
+                    onHover: {
+                      enable: true,
+                      mode: "repulse",
+                    },
+                    resize: true,
+                  },
+                  modes: {
+                    push: {
+                      quantity: 4,
+                    },
+                    repulse: {
+                      distance: 200,
+                      duration: 0.4,
+                    },
+                  },
+                },
+                particles: {
+                  color: {
+                    value: "#ffffff",
+                  },
+                  links: {
+                    color: "#ffffff",
+                    distance: 150,
+                    enable: true,
+                    opacity: 0.5,
+                    width: 1,
+                  },
+                  collisions: {
+                    enable: true,
+                  },
+                  move: {
+                    directions: "none",
+                    enable: true,
+                    outModes: {
+                      default: "bounce",
+                    },
+                    random: false,
+                    speed: 6,
+                    straight: false,
+                  } ,
+                  number: {
+                    density: {
+                      enable: true,
+                      area: 800,
+                    },
+                    value: 80,
+                  },
+                  opacity: {
+                    value: 0.5,
+                  },
+                  shape: {
+                    type: "circle",
+                  },
+                  size: {
+                    value: { min: 1, max: 5 },
+                  },
+                },
+                detectRetina: true,
+              }),
+            []
+        ),
+        lightTheme = () => {
+            containerRef.current?.loadTheme("light");
+        },
+        darkTheme = () => {
+            containerRef.current?.loadTheme("dark");
+        };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <h1>
+                   Fuck you
+                </h1>
+               
+                
+            </header>
+            {init && (
+                <Particles
+                    id="tsparticles"
+                    particlesLoaded={particlesLoaded}
+                    options={options}
+                />
+            )}
+        </div>
+    );
+}
+
+export default App;
